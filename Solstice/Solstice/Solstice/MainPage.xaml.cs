44c77fb091372a0a4ca2b5237fc262d736f5f0aa
Anthony Moore <amooretmp@gmail.com>
2018-10-29 16:14:10 +0000

    Added GPS request

--- Solstice/Solstice/Solstice/MainPage.xaml.cs
+++ Solstice/Solstice/Solstice/MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Xamarin.Essentials;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+
 
 namespace Solstice
 {
@@ -13,8 +15,10 @@ namespace Solstice
         {
             InitializeComponent();
             SetupImagesOnPage();
+            GetGPS();
         }
 
+        //images method
         private void SetupImagesOnPage()
         {
             // get the assembly and file location
@@ -22,6 +26,43 @@ namespace Solstice
             string strFilename = "Solstice.Assets.Images.mysunflower.png";
             //output background image
             imageBackground.Source = ImageSource.FromResource(strFilename, assembly);
+        }
+
+        //gps method
+        private async void GetGPS()
+        {
+            //variables
+            double lat;
+            double lon;
+
+            //request user permission for geolocation
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    lat = location.Latitude;
+                    lon = location.Longitude;
+
+                    lblLatitude.Text = "Latitude: " + Math.Round(lat, 2);
+                    lblLongitude.Text = "Longitude: " + Math.Round(lon, 2);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+
+            
         }
     }
 }
