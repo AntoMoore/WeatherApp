f6240a7a347bbb6c12ae3df677cda2af080ac592
Anthony Moore <amooretmp@gmail.com>
2018-11-28 17:42:46 +0000

    added compass icons

--- Solstice/Solstice/Solstice/MainPage.xaml.cs
+++ Solstice/Solstice/Solstice/MainPage.xaml.cs
@@ -7,7 +7,8 @@ using Xamarin.Forms;
 using Xamarin.Essentials;
 using System.Net.Http;
 using Newtonsoft.Json.Linq;
-
+using Xamarin.Forms.StyleSheets;
+using System.Reflection;
 
 namespace Solstice
 {
@@ -24,27 +25,29 @@ namespace Solstice
         float fTempLow = 0;
         float speedKph = 0;
         float speedMph = 0;
-        //class object
+
+        //class objects
         Calculations myCalc = new Calculations();
+        ImageGenerator myImg = new ImageGenerator();
 
+        //main constructors
         public MainPage()
-        {
-            //constructors
+        {            
             InitializeComponent();
-            SetupBackground();
-            GetGps();           
+            SetupStyling();
+            GetGps();
+            
         }
 
-        //images embedded reasource
-        private void SetupBackground()
+
+        //load embedded CSS
+        private void SetupStyling()
         {
-            // get the assembly and file location
-            var assembly = typeof(MainPage);
-            string strFilename = "Solstice.Assets.Images.backgroundimg.png";
-            //output background image
-            imageBackground.Source = ImageSource.FromResource(strFilename, assembly);
+            this.Resources.Add(StyleSheet.FromAssemblyResource(
+            IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly,
+            "Solstice.Assets.style.css"));
         }
-        
+
         //gps method
         public async void GetGps()
         {
@@ -72,28 +75,51 @@ namespace Solstice
                     //test if responseCurrent is not empty
                     if(responseCurrent != null)
                     {
+                        //JSON object
                         var localWeather = JObject.Parse(responseCurrent);
 
                         //variables created from the api data                       
                         var city = localWeather["name"];
                         var kTemp = localWeather["main"]["temp"];
-                        var weatherType = localWeather["weather"][0]["description"];
+                        var weatherType = localWeather["weather"][0]["main"];
                         var windSpeed = localWeather["wind"]["speed"];
                         var windDirection = localWeather["wind"]["deg"];
+
+                        //check if wind Direction is null (ie. There is no wind)
+                        if(windDirection == null)
+                        {
+                            windDirection = 0;
+                        }
                                               
-                        //calculation methods                       
+                        //calculation class methods                       
                         cTemp = myCalc.toCelsius((float)kTemp);
                         fTemp = myCalc.toFahrenheit((float)kTemp);
                         speedKph = myCalc.toKilometers((float)windSpeed);
                         speedMph = myCalc.toMiles((float)windSpeed);
 
-                        //outputs Top Grid
+                        //ImageGenerator class method - BACKGROUND
+                        var assembly = typeof(MainPage);
+                        string strFilename = myImg.getBackgroundUrl((float)cTemp, (string)weatherType);
+                        imageBackground.Source = ImageSource.FromResource(strFilename, assembly);
+
+                        //ImageGenerator class method - ICONS (current weather)
+                        string strFilename1 = myImg.getIconUrl((string)weatherType);
+                        tpMid.Source = ImageSource.FromResource(strFilename1, assembly);
+
+                        //ImageGenerator class method - COMPASS
+                        string strFilename2 = myImg.getCompassUrl((float)windDirection);
+                        tpRight.Source = ImageSource.FromResource(strFilename2, assembly);
+
+                        //white transparent tiles
+                        string strFilename3 = "Solstice.Assets.Images.whiteBackground.png";
+                        transparentImage1.Source = ImageSource.FromResource(strFilename3, assembly);
+                        transparentImage2.Source = ImageSource.FromResource(strFilename3, assembly);
+
+                        // Data outputs Top Grid
                         tpLeft.Text = city.ToString();
-                        tpMid.Text = Math.Round(cTemp, 2).ToString() + "°C";
-                        tpRight.Text = "Image Here";
-                        btmLeft.Text = Math.Round(speedKph, 2).ToString() + " Km/h";
-                        btmMid.Text = windDirection.ToString() + "°Deg";
-                        btmRight.Text = weatherType.ToString();
+                        btmLeft.Text = Math.Round(cTemp, 1).ToString() + " °C";
+                        btmRight.Text = Math.Round(speedKph, 1).ToString() + " Km/h";                      
+                        btmMid.Text = weatherType.ToString();
 
                         //DEBUG
                         //myOutput.Text = "DEBUG - Wind: " + Math.Round(speedKph, 2).ToString() + "Km/h " + city.ToString() + "\n " + weatherType.ToString() + " Temp: " + Math.Round(cTemp, 2).ToString() + "­°C"; 
@@ -113,28 +139,35 @@ namespace Solstice
                     //test if responseForecast is not empty
                     if (responseForecast != null)
                     {
+                        //JSON object
                         var localForecast = JObject.Parse(responseForecast);
-                        //make label object
+
+                        //make label & image object
                         Label label;
+                        Image image;
 
                         //make loop for 5 day forecast
                         for(int i = 0; i < 5; i++)
                         {
                             //variables created from the api data
-                            var theDescription = localForecast["list"][i]["weather"][0]["description"];
+                            var theDescription = localForecast["list"][i]["weather"][0]["main"];
                             var highTemp = localForecast["list"][i]["main"]["temp_max"];
                             var lowTemp = localForecast["list"][i]["main"]["temp_min"];
 
-                            //calculation methods                       
+                            //calculation class methods                       
                             cTempHigh = myCalc.toCelsius((float)highTemp);
                             cTempLow = myCalc.toCelsius((float)lowTemp);
                             fTempHigh = myCalc.toFahrenheit((float)highTemp);
                             fTempLow = myCalc.toFahrenheit((float)lowTemp);
 
+                            //ImageGenerator class method - ICONS (forecast)
+                            string strFilename = myImg.getIconUrl((string)theDescription);
+                            var assembly = typeof(MainPage);
+
                             //top row
-                            if ( (label = (Label)FindByName("top_" + (i + 1).ToString())) != null )
+                            if ((image = (Image)FindByName("top_" + (i + 1).ToString())) != null)
                             {
-                                label.Text = theDescription.ToString();                               
+                                image.Source = ImageSource.FromResource(strFilename, assembly);
                             }
 
                             //middle row
